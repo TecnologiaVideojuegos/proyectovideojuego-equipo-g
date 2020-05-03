bd3ef3f156ad142289f31da5547e31b8842bb2cb
Javier-Carrizosa-Bermejo <60792391+Javier-Carrizosa-Bermejo@users.noreply.github.com>
2020-05-03 19:41:23 +0200

    Update Enemigos.py

--- Código-Javi/Enemigos.py
+++ Código-Javi/Enemigos.py
@@ -11,14 +11,15 @@ class Enemy():  # solo existe para ahorrar código
 class Mosquito(Enemy):  # Clase Mosquito
     def __init__(self):
         super().__init__()
-        self.sprite = [arcade.Sprite("sprites/Fly.png", center_x=self.cor_x, center_y=self.cor_y),
-                       # Lista que contiene los 3 sprites necesarios para la animación
-                       arcade.Sprite("sprites/FlyBack.png", center_x=self.cor_x, center_y=self.cor_y),
-                       arcade.Sprite("sprites/FlyBack.png", center_x=self.cor_x, center_y=self.cor_y)]
+        self.sprite_animacion = [arcade.Sprite("sprites/Fly.png", center_x=self.cor_x, center_y=self.cor_y),
+                                 # Lista que contiene los 3 sprites necesarios para la animación
+                                 arcade.Sprite("sprites/FlyBack.png", center_x=self.cor_x, center_y=self.cor_y),
+                                 arcade.Sprite("sprites/FlyBack.png", center_x=self.cor_x, center_y=self.cor_y)]
 
         self.direc = random.randint(0, 1)  # Variable aleatoria que marcará el sentido inicial del enemigo al aparecer
         self.contador = 0  # Importante para la animación
         self.lista_balas = arcade.SpriteList()  # Lista que contendrá las balas
+        self.sprite = self.sprite_animacion[0]
 
     def movimiento(self):
         if self.direc == 1:  # cambia las coordenadas del enemigo
@@ -33,10 +34,11 @@ class Mosquito(Enemy):  # Clase Mosquito
         elif self.cor_x > 1280:
             self.direc = 0
 
-        self.prueba = self.sprite[self.contador]  # Seleccionamos el sprite a través del contador, de esta forma en cada interacción del bucle este sprite cambiará
+        self.sprite = self.sprite_animacion[
+            self.contador]  # Seleccionamos el sprite a través del contador, de esta forma en cada interacción del bucle este sprite cambiará
         # de esta forma se consigue un mejor rendimiento
-        self.prueba.center_x = self.cor_x  # reseteamos coordenadas ya que las hemos cambiado
-        self.prueba.center_y = self.cor_y
+        self.sprite.center_x = self.cor_x  # reseteamos coordenadas ya que las hemos cambiado
+        self.sprite.center_y = self.cor_y
 
         self.contador += 1  # sumamos uno al contador, en cada interacción del bucle se sumará uno
         if self.contador == 2:  # para que no salte error "outOfIndex" reseteamos el contador a 0
@@ -49,4 +51,4 @@ class Mosquito(Enemy):  # Clase Mosquito
         for r in self.lista_balas:  # actualizamos posicion de la bala
             r.center_y -= 5
             if r.center_y <= 0:
-                self.lista_balas.remove(r)   #si la bala se sale de la pantalla, se borra de la lista
+                self.lista_balas.remove(r)  # si la bala se sale de la pantalla, se borra de la lista
